e828163ce7a478741dedfdcedc7e5fd24b58b7a8
Negikami <45533155+Negikami@users.noreply.github.com>
2022-06-14 22:21:22 +0200

    Update TestController.php
    Ajout exemple route pour une clé étrangère (Test3)

--- Controllers/TestController.php
+++ Controllers/TestController.php
@@ -5,14 +5,22 @@ use Polymathee\Attributes\Route;
    
 class TestController extends BaseController
 {
+	// Point d'accès par défaut, liste tout
 	#[Route("/Polymathee/Test")]
 	static function Test(){
 		echo "Test !";
 	}
 	
+	// Point d'accès sur la clé primaire
 	#[Route("/Polymathee/Test/{id}")]
 	static function Test2($id){
 		echo "Test " . $id;
+	}
+	
+	// Point d'accès sur une clé étrangère (matière)
+	#[Route("/Polymathee/Test/Matiere/{matiere}")]
+	static function Test3($matiere){
+		echo "Test par matière " . $matiere;
 	}
 }
 ?>
