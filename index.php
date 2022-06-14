be7ea31ae204d219467cdc883cc5edcb65a16f9b
Negikami <45533155+Negikami@users.noreply.github.com>
2022-06-14 22:01:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ function custom_autoloader($class) {
 spl_autoload_register('custom_autoloader');
 
 use Polymathee\Services\RoutingService;
-use Polymathee\Controllers\TestController;
 
 $routingService = new RoutingService();
 $routingService->run();
-
-
 ?>
