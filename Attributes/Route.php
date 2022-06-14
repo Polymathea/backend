9c4dc6f6fb243be2a654186fa763a3b450763502
Negikami <45533155+Negikami@users.noreply.github.com>
2022-06-14 22:09:43 +0200

    Update Route.php

--- Attributes/Route.php
+++ Attributes/Route.php
@@ -3,7 +3,7 @@ namespace Polymathee\Attributes;
 
 use \Attribute;
 
-#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD)]
+#[Attribute(Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
 class Route
 {   
     public function __construct(string $path, string $method = "GET")
